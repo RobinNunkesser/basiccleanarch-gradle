b1b40f3fc8215ba3494ac2fe8cedc68d310e44a7
Robin Nunkesser <robin.nunkesser@hshl.de>
2019-09-06 18:49:18 +0200

    Changed Displayer -> Version 1.0

--- src/main/kotlin/de/hshl/isd/basiccleanarch/Displayer.kt
+++ src/main/kotlin/de/hshl/isd/basiccleanarch/Displayer.kt
@@ -4,12 +4,20 @@ package de.hshl.isd.basiccleanarch
  * A `Displayer` is used to display the result of a use case that
  * changes the displayed view.
  */
-interface Displayer {
+interface Displayer<ViewModel> {
 
     /**
-     * Displays data from a ViewModel or an Error.
+     * Displays data from a ViewModel.
      *
-     * @param result the result to display.
+     * @param success the result to display.
      */
-    fun display(result: Response)
+    fun display(success: ViewModel)
+
+    /**
+     * Displays an Error.
+     *
+     * @param error the error to display.
+     */
+    fun display(error: Throwable)
+
 }
